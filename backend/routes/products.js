8c171bebed9b5b03dc3511025a514201fbb50fb2
Nithin M.K <nithinmkurien@gmail.com>
2022-07-07 20:57:23 +0530

    04-fetch-data

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -57,18 +57,42 @@ const products = [
 
 // Get list of products products
 router.get('/', (req, res, next) => {
-  // Return a list of dummy products
-  // Later, this data will be fetched from MongoDB
-  const queryPage = req.query.page;
-  const pageSize = 5;
-  let resultProducts = [...products];
-  if (queryPage) {
-    resultProducts = products.slice(
-      (queryPage - 1) * pageSize,
-      queryPage * pageSize
-    );
-  }
-  res.json(resultProducts);
+  // const queryPage = req.query.page;
+  // const pageSize = 5;
+  // let resultProducts = [...products];
+  // if (queryPage) {
+  //   resultProducts = products.slice(
+  //     (queryPage - 1) * pageSize,
+  //     queryPage * pageSize
+  //   );
+  // }
+  MongoClient.connect(
+    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0.kkn2g.mongodb.net/shop?retryWrites=true&w=majority'
+  )
+    .then(client => {
+      const products = [];
+      client
+        .db()
+        .collection('products')
+        .find()
+        .forEach(productDoc => {
+          productDoc.price = productDoc.price.toString();
+          products.push(productDoc);
+        })
+        .then(result => {
+          client.close();
+          res.status(200).json(products);
+        })
+        .catch(err => {
+          console.log(err);
+          client.close();
+          res.status(500).json({ message: 'An error occurred.' });
+        });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: 'An error occurred.' });
+    });
 });
 
 // Get single product
@@ -109,6 +133,7 @@ router.post('', (req, res, next) => {
     })
     .catch(err => {
       console.log(err);
+      res.status(500).json({ message: 'An error occurred.' });
     });
 });
 
