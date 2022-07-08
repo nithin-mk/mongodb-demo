345155ac835dfcdf0bec8c6fa702755b5445dde5
Nithin M.K <nithinmkurien@gmail.com>
2022-07-08 15:54:29 +0530

    10-added-login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -13,13 +13,30 @@ const createToken = () => {
 router.post('/login', (req, res, next) => {
   const email = req.body.email;
   const pw = req.body.password;
-  // Check if user login is valid
-  // If yes, create token and return it to client
-  const token = createToken();
+  db.getDb()
+    .db()
+    .collection('users')
+    .findOne({ email: email })
+    .then(userDoc => {
+      return bcrypt.compare(pw, userDoc.password);
+    })
+    .then(result => {
+      if (!result) {
+        throw Error();
+      }
+      const token = createToken();
+      res.status(200).json({
+        message: 'Authentication succeeded.',
+        token: token
+      });
+    })
+    .catch(err => {
+      res.status(401).json({
+        message: 'Authentication failed, invalid username or password.'
+      });
+    });
+
   // res.status(200).json({ token: token, user: { email: 'dummy@dummy.com' } });
-  res
-    .status(401)
-    .json({ message: 'Authentication failed, invalid username or password.' });
 });
 
 router.post('/signup', (req, res, next) => {
