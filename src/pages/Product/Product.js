df9d2dbe296b31ceda5b8fe87a6c0481e4d95bf1
Nithin M.K <nithinmkurien@gmail.com>
2022-07-14 11:23:44 +0530

    03-fetch-product

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { Stitch, RemoteMongoClient } from 'mongodb-stitch-browser-sdk';
+import BSON from 'bson';
 
 import './Product.css';
 
@@ -7,15 +8,27 @@ class ProductPage extends Component {
   state = { isLoading: true, product: null };
 
   componentDidMount() {
-    axios
-      .get('http://localhost:3100/products/' + this.props.match.params.id)
+    const mongodb = Stitch.defaultAppClient.getServiceClient(
+      RemoteMongoClient.factory,
+      'mongodb-atlas'
+    );
+    mongodb
+      .db('shop')
+      .collection('products')
+      .find({ _id: new BSON.ObjectID(this.props.match.params.id) })
+      .asArray()
       .then(productResponse => {
-        this.setState({ isLoading: false, product: productResponse.data });
+        const product = productResponse[0];
+        product._id = product._id.toString();
+        product.price = product.price.toString();
+        this.setState({ isLoading: false, product: product });
       })
       .catch(err => {
         this.setState({ isLoading: false });
         console.log(err);
-        this.props.onError('Loading the product failed. Please try again later');
+        this.props.onError(
+          'Loading the product failed. Please try again later'
+        );
       });
   }
 
